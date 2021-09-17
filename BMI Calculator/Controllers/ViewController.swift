81493caa3a8e2a916d39ae77c4ea8de9915cfc81
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-09-17 11:28:50 +0200

    implemented logic for UISlider.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightText: UILabel!
+    @IBOutlet weak var weightText: UILabel!
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightText.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightText.text = "\(String(format: "%.0f", sender.value))kg"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
